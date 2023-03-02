2bcc0fbd8acf11a70298c7de68d9cc59719039cb
Bhavish77 <114435833+Bhavish77@users.noreply.github.com>
2023-03-02 09:31:15 +0530

    Update lengthofstring.cpp

--- cpp/strings/lengthofstring.cpp
+++ cpp/strings/lengthofstring.cpp
@@ -1,1 +1,25 @@
 
+#include <iostream>
+#include<vector>
+using namespace std;
+
+
+int main()
+{
+  vector<int> arr{4,5,2,3,1};
+  int n = arr.size();
+  int length = 0;
+  int i = 0;
+
+  while(arr[i] != 0)
+    {
+      length++;
+      i++;
+    }
+  cout << length << endl;
+
+  
+
+  
+  
+}
