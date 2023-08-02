0a18c2e9e795bba1603ae3e40ef416041b83b8a0
Bhavish77 <114435833+Bhavish77@users.noreply.github.com>
2023-08-02 15:12:42 +0530

    Create bubbleSort.js

---
+++ js/sorting/bubbleSort.js
@@ -0,0 +1,24 @@
+function bubbleSort(arr) {
+    const n = arr.length;
+
+    for (let round = 1; round < n; round++) {
+        let swapCount = 0;
+        for (let j = 0; j < n - round; j++) {
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+                swapCount++;
+            }
+        }
+        if (swapCount === 0) {
+            // sort ho chuka hai, no need to check in further rounds
+            break;
+        }
+    }
+
+    return arr;
+}
+
+// Test the function
+const arr = [10, 1, 7, 6, 14, 9];
+const sortedArr = bubbleSort(arr);
+console.log(sortedArr);
