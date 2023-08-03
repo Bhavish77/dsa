f6de53a4afea0cd1b7c17a969a8deea3e4ca37cb
Bhavish77 <114435833+Bhavish77@users.noreply.github.com>
2023-08-03 15:13:07 +0530

    Update selectionSort.js

--- js/sorting/selectionSort.js
+++ js/sorting/selectionSort.js
@@ -2,16 +2,16 @@ function selectionSort(arr) {
   const n = arr.length;
 
   for (let i = 0; i < n - 1; i++) {
-    let minIndex = i;
+    let minIndex = i;       //get the min index
 
     for (let j = i + 1; j < n; j++) {
-      if (arr[j] < arr[minIndex]) {
+      if (arr[j] < arr[minIndex]) {     //update minindex
         minIndex = j;
       }
     }
 
     // Swap elements at i and minIndex
-    [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];       //swap current with minimum element found
   }
 
   return arr;
